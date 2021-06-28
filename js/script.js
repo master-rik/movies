cbee423414c8435e5d0172d46f547c328b0426f8
Yuriy <ykantakuzin@bk.ru>
2021-06-28 16:37:30 +0300

    Работа с DOM

--- js/script.js
+++ js/script.js
@@ -1,98 +1,53 @@
-/* Задание на урок:
+/* Задания на урок:
 
-1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
-перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
-Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
-2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
-переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+2) Изменить жанр фильма, поменять "комедия" на "драма"
 
-3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
-Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
-при помощи метода forEach вывести в консоль сообщения в таком виде:
-"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
 
 'use strict';
 
-
-const personalMovieDB = {
-	count: 0,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false,
-	start: () => {
-		personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-		while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-			personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-		}
-	},
-	rememberMyFilms: () => {
-		for (let i = 0; i < personalMovieDB.count; i++) {
-			const movie = prompt('Один из последних просмотренных фильмов?', ''),
-				rating = prompt('На сколько оцените его?', '');
-			if (movie != null && rating != null && movie != '' &&
-				rating != '' && movie.length < 50) {
-				personalMovieDB.movies[movie] = rating;
-				console.log('DONE');
-			} else {
-				console.log('Error');
-				i--;
-			}
-		}
-	},
-	detectPersonalLevel: () => {
-		if (personalMovieDB.count < 10) {
-			console.log("Просмотрено довольно мало фильмов");
-		} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-			console.log("Вы классический зритель");
-		} else if (personalMovieDB.count >= 30) {
-			console.log("Вы киноман");
-		} else {
-			console.log("Произошла ошибка");
-		}
-	},
-	writeYourGenres: () => {
-		for (let i = 1; i <= 3; i++) {
-			let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-			if (genre == '' || genre === null) {
-				console.log('Вы ввели не корректные данные');
-				i--;
-			} else {
-				personalMovieDB.genres[i - 1] = genre;
-			}
-		}
-		personalMovieDB.genres.forEach((item, i) => {
-			console.log(` Любимый жанр ${i+1} - это ${item} `);
-		});
-	},
-	showMyDB: (hiden) => {
-		if (!hiden) {
-			console.log(personalMovieDB);
-		} else {
-			console.log('доступ ограничен!');
-		}
-	},
-	toggleVisibleMyDB: () => {
-		if (personalMovieDB.privat) {
-			personalMovieDB.privat = false;
-		} else {
-			personalMovieDB.privat = true;
-		}
-	}
+const movieDB = {
+	movies: [
+		"Логан",
+		"Лига справедливости",
+		"Ла-ла лэнд",
+		"Одержимость",
+		"Скотт Пилигрим против..."
+	]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+	bg = document.querySelector(`.promo__bg`),
+	genre = bg.querySelector('.promo__genre'),
+	movielist = document.querySelector(`.promo__interactive-list`);
 
+//1
+adv.forEach(item => {
+	item.remove();
+});
+//2
+genre.textContent = 'драма';
 
-/* personalMovieDB.start();
-personalMovieDB.rememberMyFilms();
-personalMovieDB.detectPersonalLevel();
-personalMovieDB.writeYourGenres();
-personalMovieDB.showMyDB();
-personalMovieDB.toggleVisibleMyDB(); */
+//3
+bg.style.backgroundImage = `url('img/bg.jpg')`;
 
-/* console.log(personalMovieDB.count, `сколько просмотренно фильмов`);
-console.log(personalMovieDB.movies);
-console.log(personalMovieDB.genres);
-console.log(personalMovieDB.showMyDB);
-console.log(personalMovieDB.privat); */
+//4
+movielist.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+	movielist.innerHTML += `
+	<li class="promo__interactive-item">${i+1}  ${film}
+		<div class="delete"></div>
+	</li>
+	`;
+});
